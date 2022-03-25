88690c0aadb9ed6a173aaf454604f6a6ffb24ee1
michinarikake <99639660+michinarikake@users.noreply.github.com>
2022-03-25 14:58:10 +0900

    Add files via upload

--- rotate.ino
+++ rotate.ino
@@ -4,16 +4,16 @@
 #include <Wire.h>
 #include <DFRobot_QMC5883.h>
 #include <Servo.h>
-
+Servo myservo;
 const int motor11 = 2;
 const int motor12 = 3;
 const int motor21 = 6;
 const int motor22 = 5;
-const int PWM = 10;
 const int pinA = 7;
 const int pinB = 8;
-Servo myservo;
 const int SV_PIN = 9;
+const int PWMa = A4;
+const int PWMb = A5;
 int minX = 0;
 int maxX = 0;
 int minY = 0;
@@ -25,7 +25,7 @@ int i = 0;
 DFRobot_QMC5883 compass;
 
 void setup_servo() {
-  myservo.attach(SV_PIN, 500, 2400);  // サーボの割当(パルス幅500~2400msに指定)距離せんさ
+  myservo.attach(SV_PIN, 500, 2400);  // サーボの割当(パルス幅500~2400msに指定)
 }
 
 void setup_qmc5883() {
@@ -41,10 +41,10 @@ void setup_qmc5883() {
 int qmc5883() {
   double X = 0;
   double Y = 0;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 5; i++) {
     Vector mag = compass.readRaw();
-    X += mag.XAxis * 0.000105862 + 0.319926492;
-    Y += mag.YAxis * 0.000113317 - 0.21266429 + 0.0000000000000001;
+    X += mag.XAxis * 0.000118522 + 0.232580541;
+    Y += mag.YAxis * 0.000115348 + 0.211307431;
   }
   int theta;
   theta =  atan2 ( Y, X ) * 57.2958;
@@ -56,9 +56,11 @@ void setup_motor() {
   pinMode(motor12, OUTPUT);
   pinMode(motor21, OUTPUT);
   pinMode(motor22, OUTPUT);
-  pinMode(PWM, OUTPUT);
   pinMode (pinA, INPUT);
   pinMode (pinB, INPUT);
+  pinMode(PWMb, OUTPUT);
+  pinMode(PWMa, OUTPUT);
+  Serial.println("setup done");
 }
 
 void right_rotate( int kakudo ) {
@@ -66,49 +68,54 @@ void right_rotate( int kakudo ) {
   microt = millis();
   int theta0 = qmc5883();
   int theta = theta0;
-  
+  analogWrite( PWMb, 249 );
+  analogWrite( PWMa, 254 );
   digitalWrite(motor11, HIGH);
   digitalWrite(motor22, LOW);
   digitalWrite(motor12, LOW);
   digitalWrite(motor21, HIGH);
-  analogWrite( PWM, 249 );
+  Serial.println("motor done");
 
   while (theta - theta0 < kakudo) {
     if (qmc5883() < theta0) {
       theta = qmc5883() + 360;
+      Serial.println(theta);
     }
     else {
       theta = qmc5883();
+      Serial.println(theta);
     }
   }
   digitalWrite(motor11, LOW);
   digitalWrite(motor21, LOW);
+  double omega = (theta - theta0) / microt;
+  theta = qmc5883();
+  int difference = theta - theta0 - kakudo;
+  int t = difference / omega;
 
   while (abs(theta - theta0 - kakudo) > 5) {
-    double omega = (theta - theta0) / microt;
-    theta = qmc5883();
-    int difference = theta - theta0 - kakudo;
-    int t = difference / omega;
 
     if ( theta - theta0 - kakudo > 0) {
+      analogWrite( PWMb, 249 );
+      analogWrite( PWMa, 254 );
       digitalWrite(motor11, LOW);
       digitalWrite(motor22, HIGH);
       digitalWrite(motor12, HIGH);
       digitalWrite(motor21, LOW);
-      analogWrite( PWM, 249 );
       delay(t);
 
       digitalWrite(motor12, LOW);
       digitalWrite(motor22, LOW);
     }
     else {
+      analogWrite( PWMb, 249 );
+      analogWrite( PWMa, 254 );
       digitalWrite(motor11, HIGH);
       digitalWrite(motor22, LOW);
       digitalWrite(motor12, LOW);
       digitalWrite(motor21, HIGH);
-      analogWrite( PWM, 249 );
       delay(t);
-      
+
       digitalWrite(motor11, LOW);
       digitalWrite(motor21, LOW);
 
@@ -121,12 +128,12 @@ void left_rotate( int kakudo ) {
   int theta = theta0;
   unsigned long microt = 0;
   microt = millis();
-  
+  analogWrite( PWMb, 249 );
+  analogWrite( PWMa, 254 );
   digitalWrite(motor11, LOW);
   digitalWrite(motor22, HIGH);
   digitalWrite(motor12, HIGH);
   digitalWrite(motor21, LOW);
-  analogWrite( PWM, 249 );
   while (theta0 - theta < kakudo) {
     if (qmc5883() > theta0) {
       theta = qmc5883() - 360;
@@ -138,31 +145,33 @@ void left_rotate( int kakudo ) {
   digitalWrite(motor12, LOW);
   digitalWrite(motor22, LOW);
 
-  
+
   while (abs(theta - theta0 - kakudo) > 5) {
     double omega = (theta - theta0) / microt;
     theta = qmc5883();
     int difference = theta - theta0 - kakudo;
     int t = difference / omega;
     if ( theta - theta0 - kakudo > 0) {
+      analogWrite( PWMb, 249 );
+      analogWrite( PWMa, 254 );
       digitalWrite(motor11, LOW);
       digitalWrite(motor22, HIGH);
       digitalWrite(motor12, HIGH);
       digitalWrite(motor21, LOW);
-      analogWrite( PWM, 249 );
       delay(t);
 
       digitalWrite(motor12, LOW);
       digitalWrite(motor22, LOW);
     }
     else {
+      analogWrite( PWMb, 249 );
+      analogWrite( PWMa, 254 );
       digitalWrite(motor11, HIGH);
       digitalWrite(motor22, LOW);
       digitalWrite(motor12, LOW);
       digitalWrite(motor21, HIGH);
-      analogWrite( PWM, 249 );
       delay(t);
-      
+
       digitalWrite(motor11, LOW);
       digitalWrite(motor21, LOW);
 
@@ -170,13 +179,11 @@ void left_rotate( int kakudo ) {
   }
 }
 
-
 void setup() {
+  setup_qmc5883();
   setup_motor();
-  setup_qmc5883;
-  setup_servo();
+  Serial.begin(9600);
 }
 void loop() {
-  myservo.write(60);
-  left_rotate(90);
+  right_rotate( 90 );
 }
